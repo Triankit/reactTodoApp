8af8624d91d4c484a1f696dd5496102ded890349
Ankit Tripathi <tripathi.ankit83@gmail.com>
2025-12-29 18:41:36 +0530

    Implement basic task management features in the To Do app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [task, setTask] = useState([]);
+	const [userInput, setUserInput] = useState("");
+	const [editId, setEditId] = useState(null);
+	const [editTask, setEditTask] = useState("");
+	const [checkedTasks, setCheckedTasks] = useState(false);
+	function handleSubmit(event) {
+		event.preventDefault();
+		if (userInput === "") return;
+		const newTask = {
+			id: Date.now(),
+			name: userInput,
+			status: "incomplete",
+		};
+		setTask((task) => [...task, newTask]);
+		setUserInput("");
+	}
+
+	function toogleEdit(id) {
+		setEditId(id);
+		task.map((ele) => {
+			if (ele.id === id) {
+				return setEditTask(ele.name);
+			}
+			return null;
+		});
+	}
+
+	function handleEdit(id) {
+		const updatedTasks = task.map((ele) => {
+			if (ele.id === id) {
+				return { ...ele, name: editTask };
+			}
+			return ele;
+		});
+		setTask(updatedTasks);
+		setEditId(null);
+	}
+
+	function handleDelete(id) {
+		const filterArray = task.filter((ele) => {
+			return ele.id !== id;
+		});
+		setTask(filterArray);
+	}
+
+	function handleCheckboxChange(id) {
+		const checkedTask = task.find((ele) => ele.id === id);
+		if (checkedTask) {
+			checkedTask.status =
+				checkedTask.status === "complete" ? "incomplete" : "complete";
+			setTask([...task]);
+		}
+		setCheckedTasks(!checkedTasks);
+	}
+
+	return (
+		<div className='App'>
+			<h1>To do App</h1>
+			<div>
+				<input
+					value={userInput}
+					onChange={(e) => setUserInput(e.target.value)}
+					placeholder='Enter the task'
+				/>
+				<button
+					type='submit'
+					onClick={handleSubmit}
+					style={{ marginLeft: "5px" }}
+				>
+					{" "}
+					Add Task{" "}
+				</button>
+				<hr />
+			</div>
+			<div>
+				<ul>
+					{task.map((ele) => (
+						<div key={ele.id}>
+							{editId === ele.id ? (
+								<div>
+									<input
+										value={editTask}
+										autoFocus={true}
+										onChange={(e) => {
+											setEditTask(e.target.value);
+										}}
+									/>
+									<button onClick={() => handleEdit(ele.id)}>✅</button>
+									<button onClick={() => setEditId(null)}>❌</button>
+								</div>
+							) : (
+								<>
+									<input
+										type='checkbox'
+										checked={ele.status === "complete"}
+										onChange={() => {
+											handleCheckboxChange(ele.id);
+										}}
+									/>
+									<text
+										style={{
+											textDecoration:
+												ele.status === "complete" ? "line-through" : "",
+										}}
+									>
+										{ele.name}
+									</text>
+								</>
+							)}
+							<button
+								onClick={() => toogleEdit(ele.id)}
+								style={{ marginLeft: "5px" }}
+							>
+								✍🏻
+							</button>
+							<button
+								onClick={() => handleDelete(ele.id)}
+								style={{ marginLeft: "5px" }}
+							>
+								🗑
+							</button>
+						</div>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
